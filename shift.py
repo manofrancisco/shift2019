c508a047c8d062e5d530e680d8a153872f4be8cb
manofrancisco <francisco4martinsferreira@gmail.com>
2019-04-06 19:02:23 +0100

    aa

--- shift.py
+++ shift.py
@@ -1,23 +1,47 @@
-import flask
+from flask import Flask,request as r,Response
 import requests
 from pathlib import Path
 from random import SystemRandom
-app = flask.Flask(__name__)
+import json
+from netifaces import *
+
+
+
+
+from stringutils import BMSearch
+
+app = Flask(__name__)
 
 digits = ''
-id = 0
-server_address = 'http://127.0.0.1:8000'
+def main():
+    global myaddress
+    global contaclist
+    contaclist={}
+    myaddress='0'
+
 
 
 @app.route('/search/<string>', methods=['GET'])
 def search(string):
     return searchstring(string)
 
+@app.route('/register/<ip>',methods=['GET','POST'])
+def register(ip):
+    if(r.method =='POST' ):
+        newid = len(contactlist.keys())
+        contactlist[str(newid)] = ip
+        return Response(status=200)
+    if(r.method == 'GET'):
+        #add to contactlist and return
+        contactlist
+        newid = len(contactlist.keys())
+        contactlist[str(newid)] = ip
+        return json.dumps(contactlist, ensure_ascii=False)
 
 #/get/index?length=10
 @app.route('/get/<index>', methods=['GET'])
 def get(index):
-    length = flask.request.args.get('length', default=10, type=int)
+    length = r.args.get('length', default=10, type=int)
     return getstring(index,length)
 
 
@@ -43,94 +67,45 @@ def get_digits():
     clientfile.write(string)
     clientfile.close()
 
+def propagate(myaddress):
+    for k,v in contactlist:
+        address = 'http://' + v + '/register/' + myaddress
+        requests.post(address)
 
-def get_info():
+def register():
     print('getting info')
-    #r = requests.get(server_address+'/Registration')
-    #return r.text
+    ifaceName = interfaces()[6]
+    addresses = [i['addr'] for i in ifaddresses(ifaceName).setdefault(AF_INET, [{'addr': 'No IP addr'}])]
+    myaddress = addresses[0]
+    address = 'http://'+contactlist.keys[0]+'/register/'+myaddress
+    r = requests.get(address)
+    contactlist= r.json()
+    propagate(myaddress)
     return '0'
 
 def setupclient():
-    info = get_info()
+    info = register()
     clientfile = open('clientid.txt', 'w')
     clientfile.write(info)
     clientfile.close()
 
 def is_installed():
-    file = Path('clientid.txt')
+    file = Path('digits.pi')
     if not file.is_file():
         return False
     return True
 
 
-# Generate the Bad Character Skip List
-def generateBadCharShift(term):
-    skipList = {}
-    for i in range(0, len(term) - 1):
-        skipList[term[i]] = len(term) - i - 1
-    return skipList
-
-
-# Generate the Good Suffix Skip List
-def findSuffixPosition(badchar, suffix, full_term):
-    for offset in range(1, len(full_term) + 1)[::-1]:
-        flag = True
-        for suffix_index in range(0, len(suffix)):
-            term_index = offset - len(suffix) - 1 + suffix_index
-            if term_index < 0 or suffix[suffix_index] == full_term[term_index]:
-                pass
-            else:
-                flag = False
-        term_index = offset - len(suffix) - 1
-        if flag and (term_index <= 0 or full_term[term_index - 1] != badchar):
-            return len(full_term) - offset + 1
-
-
-def generateSuffixShift(key):
-    skipList = {}
-    buffer = ""
-    for i in range(0, len(key)):
-        skipList[len(buffer)] = findSuffixPosition(key[len(key) - 1 - i], buffer, key)
-        buffer = key[len(key) - 1 - i] + buffer
-    return skipList
-
-
-# Actual Search Algorithm
-def BMSearch(needle):
-    haystack = ''
-    with open('digits.pi', 'r') as myfile:
-        haystack = myfile.read()
-    print(haystack)
-    goodSuffix = generateSuffixShift(needle)
-    badChar = generateBadCharShift(needle)
-    i = 0
-    while i < len(haystack) - len(needle) + 1:
-        j = len(needle)
-        while j > 0 and needle[j - 1] == haystack[i + j - 1]:
-            j -= 1
-        if j > 0:
-            badCharShift = badChar.get(haystack[i + j - 1], len(needle))
-            goodSuffixShift = goodSuffix[len(needle) - j]
-            if badCharShift > goodSuffixShift:
-                i += badCharShift
-            else:
-                i += goodSuffixShift
-        else:
-            return i
-    return -1
-
-
 
 def setup():
+    with open('contacts.json', 'r') as infile:
+        list= json.load(infile)
+    contactlist = list
     idH = ''
-    with open('clientid.txt','r') as myfile:
-        idH = myfile.read()
-    id = idH
-    print('\n')
-    print(id)
-    print('\n')
+
 
 if __name__ == '__main__':
+    main()
     install()
     setup()
     app.run()
